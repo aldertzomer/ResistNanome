f4d4d2f8ec9e9f9969d739d1237418919f42252c
Aldert Zomer <aldertzomer@gmail.com>
2023-06-27 10:45:04 +0200

    Update getDB.sh
    changed to https://

--- database/getDB.sh
+++ database/getDB.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "This script needs wget"
-echo "Downloading databases from http://klif.uu.nl/download/metagenomics_db/"
+echo "Downloading databases from https://klif.uu.nl/download/metagenomics_db/"
 echo "Please be patient. This will take some time (but only once)"
 echo
 echo "Downloading Kraken2 db"
-wget http://klif.uu.nl/download/metagenomics_db/Kraken2_Nanodb.tar.gz
+wget https://klif.uu.nl/download/metagenomics_db/Kraken2_Nanodb.tar.gz
 echo "Downloading host filter db (Mash db)"
-wget http://klif.uu.nl/download/metagenomics_db/mash_db.tar.gz
+wget https://klif.uu.nl/download/metagenomics_db/mash_db.tar.gz
 echo "Downloading KMA db"
-wget http://klif.uu.nl/download/metagenomics_db/KMA_ResFinder.tar.gz
+wget https://klif.uu.nl/download/metagenomics_db/KMA_ResFinder.tar.gz
 
 echo "Untarring files"
 tar xzvf Kraken2_Nanodb.tar.gz
